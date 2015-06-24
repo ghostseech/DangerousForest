db61a0c7c52bfbd0d634c33437bd39604ab205e8
ghostseech <mego.mego2014@yandex.ru>
2015-06-24 13:37:01 +0300

    Initial commit

---
+++ desktop/src/com/ghstsch/dangerousforest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ghstsch.dangerousforest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ghstsch.dangerousforest.DangerousForest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DangerousForest(), config);
+	}
+}
